9d3245daaa52bf4e32abbdf51325be4a356c7e07
Jesse Pinho <jesse@jessepinho.com>
2023-06-30 18:15:06 -0700

    Do basic data fetch from Airtable in API endpoint

--- src/app/api/airtable/route.ts
+++ src/app/api/airtable/route.ts
@@ -1,13 +1,37 @@
+import Airtable from 'airtable'
 import { NextResponse } from 'next/server'
 
-export async function GET(request: Request) {
-  const res = await fetch('https://baconipsum.com/api/?type=meat-and-filler', {
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: process.env.DATA_API_KEY,
-    },
-  })
-  const data = await res.json()
+if (!process.env.AIRTABLE_API_KEY) {
+  throw new Error(
+    'Please define AIRTABLE_API_KEY in your environment and restart/redeploy this service. See the README for more detail.'
+  )
+}
 
-  return NextResponse.json(data)
+if (!process.env.AIRTABLE_BASE_ID) {
+  throw new Error(
+    'Please define AIRTABLE_BASE_ID in your environment and restart/redeploy this service. See the README for more detail.'
+  )
+}
+
+if (!process.env.AIRTABLE_TABLE_ID) {
+  throw new Error(
+    'Please define AIRTABLE_TABLE_ID in your environment and restart/redeploy this service. See the README for more detail.'
+  )
+}
+
+// The Airtable SDK automatically uses the `AIRTABLE_API_KEY` environment
+// variable when connecting to the Airtable API, but I've set it explicitly here
+// anyway to make it clear where it comes from.
+Airtable.configure({ apiKey: process.env.AIRTABLE_API_KEY })
+
+const base = new Airtable().base(process.env.AIRTABLE_BASE_ID)
+
+export async function GET(request: Request) {
+  const result = await base(process.env.AIRTABLE_TABLE_ID as string)
+    .select({
+      fields: ['Notes', 'Multi Select', 'Assignee', 'Tags'],
+    })
+    .all()
+
+  return NextResponse.json(result.map(item => item._rawJson))
 }
