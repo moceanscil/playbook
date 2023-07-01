b57e6dd213d518f1ae5dc51f7c23a12671dd1171
Jesse Pinho <jesse@jessepinho.com>
2023-06-30 19:33:09 -0700

    Install and set up @mui/material

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
-export const metadata = {
-  title: 'NeighborAide Playbook',
-  description: 'A playbook for social workers to access important resources.',
-}
+'use client'
+
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+const theme = createTheme()
 
 export default function RootLayout({
   children,
@@ -10,7 +15,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </head>
+      <body>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
   return <main>Hello, world!</main>
 }
+
+export const metadata = {
+  title: 'NeighborAide Playbook',
+  description: 'A playbook for social workers to access important resources.',
+}
