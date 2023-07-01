f38867f857607562d18418645c083a8ca6318cb9
Jesse Pinho <jesse@jessepinho.com>
2023-06-30 23:09:12 -0700

    Allow users to send resources to their neighbors

--- src/components/HomePage/Resources.tsx
+++ src/components/HomePage/Resources.tsx
@@ -1,8 +1,26 @@
-import { List, ListItem, ListItemButton, ListItemText } from '@mui/material'
+import {
+  Checkbox,
+  Fab,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from '@mui/material'
 
 import Step from './Step'
+import { useState } from 'react'
+import { Send } from '@mui/icons-material'
+import LinkButton from './LinkButton'
 
-const MOCK_RESOURCES = [
+type Resource = {
+  id: string
+  name: string
+  description: string
+  url: string
+}
+
+const MOCK_RESOURCES: Resource[] = [
   {
     id: '1',
     name: 'Test resource 1',
@@ -17,12 +35,40 @@ const MOCK_RESOURCES = [
   },
 ]
 
+const getEmailBody = (resourceIds: string[], resources: Resource[]): string =>
+  resourceIds
+    .map(resourceId => {
+      const resource = resources.find(({ id }) => resourceId === id) as Resource
+      return `- ${resource.name} (${resource.url})\n${resource.description}`
+    })
+    .join('\n\n')
+
 export default function Resources() {
+  const [selected, setSelected] = useState<string[]>([])
+
+  const handleToggle = (valueToToggle: string) =>
+    setSelected(current =>
+      current.includes(valueToToggle)
+        ? current.filter(value => value !== valueToToggle)
+        : [...current, valueToToggle]
+    )
+
+  const emailBody = getEmailBody(selected, MOCK_RESOURCES)
+
   return (
-    <Step title="Here are some resources that should help">
+    <Step title="Here are some resources for your neighbor.">
       <List>
         {MOCK_RESOURCES.map(resource => (
-          <ListItem key={resource.id}>
+          <ListItem
+            key={resource.id}
+            secondaryAction={
+              <Checkbox
+                edge="end"
+                checked={selected.includes(resource.id)}
+                onChange={() => handleToggle(resource.id)}
+              />
+            }
+          >
             <ListItemButton
               LinkComponent="a"
               href={resource.url}
@@ -37,6 +83,15 @@ export default function Resources() {
           </ListItem>
         ))}
       </List>
+
+      <LinkButton
+        disabled={!selected.length}
+        href="mailto:"
+        query={{ subject: 'Some helpful resources for you', body: emailBody }}
+        startIcon={<Send />}
+      >
+        Send
+      </LinkButton>
     </Step>
   )
 }
