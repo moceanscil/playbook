8fe086aeec29c82dfbed9cd23cf09fa5d637a1f3
Jesse Pinho <jesse@jessepinho.com>
2023-06-30 23:08:59 -0700

    Allow more props to LinkButton; handle mailto links

--- src/components/HomePage/LinkButton.tsx
+++ src/components/HomePage/LinkButton.tsx
@@ -1,21 +1,31 @@
 import { Button, SxProps } from '@mui/material'
 import Link from 'next/link'
+import { ReactNode } from 'react'
+
+const toStringWithMailFriendlySpaces = (params: URLSearchParams) =>
+  params.toString().replaceAll('+', '%20')
 
 export default function LinkButton({
+  href = '',
   query,
   children,
   disabled,
   sx,
+  startIcon,
 }: {
-  query: Record<string, string>
-  children: string
+  href?: string
+  query?: Record<string, string>
+  children: ReactNode
   disabled?: boolean
   sx?: SxProps
+  startIcon?: ReactNode
 }) {
-  const href = '?' + new URLSearchParams(query).toString()
+  if (query)
+    href += '?' + toStringWithMailFriendlySpaces(new URLSearchParams(query))
 
   return (
     <Button
+      startIcon={startIcon}
       size="large"
       sx={sx}
       disabled={disabled}
