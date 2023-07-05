87e966144d8177e9ecc1c189c93cf3d6e632a542
Jesse Pinho <jesse@jessepinho.com>
2023-07-04 18:09:43 -0700

    Use capitalized county names (since they'll eventually be passed to Airtable)

--- src/components/Playbook/County.tsx
+++ src/components/Playbook/County.tsx
@@ -4,10 +4,10 @@ import Step from './Step'
 export default function County() {
   return (
     <Step title="In which county?" step="County">
-      <LinkButton query={{ action: 'neighbor', county: 'monmouth' }}>
+      <LinkButton query={{ action: 'neighbor', county: 'Monmouth' }}>
         Monmouth
       </LinkButton>
-      <LinkButton query={{ action: 'neighbor', county: 'ocean' }}>
+      <LinkButton query={{ action: 'neighbor', county: 'Ocean' }}>
         Ocean
       </LinkButton>
     </Step>
