ac6b190450ee3541e85d6ca2d326478c198111d2
Jesse Pinho <jesse@jessepinho.com>
2023-07-04 19:05:19 -0700

    Show all counties

--- src/components/Playbook/County.tsx
+++ src/components/Playbook/County.tsx
@@ -1,15 +1,20 @@
+import { useContext } from 'react'
+
 import LinkButton from './LinkButton'
+import PlaybookContext from './PlaybookContext'
 import Step from './Step'
 
 export default function County() {
+  // TODO: Limit to just Monmouth/Ocean?
+  const { countyServedValues } = useContext(PlaybookContext)
+
   return (
     <Step title="In which county?" step="County">
-      <LinkButton query={{ action: 'neighbor', county: 'Monmouth' }}>
-        Monmouth
-      </LinkButton>
-      <LinkButton query={{ action: 'neighbor', county: 'Ocean' }}>
-        Ocean
-      </LinkButton>
+      {countyServedValues.map(county => (
+        <LinkButton query={{ action: 'neighbor', county }} key={county}>
+          {county}
+        </LinkButton>
+      ))}
     </Step>
   )
 }
