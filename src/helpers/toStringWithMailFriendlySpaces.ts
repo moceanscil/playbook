5815d09712d3fc51b7bc1bfb563ec116674ad159
Jesse Pinho <jesse@jessepinho.com>
2023-07-23 21:49:32 -0700

    Document the helper

--- src/helpers/toStringWithMailFriendlySpaces.ts
+++ src/helpers/toStringWithMailFriendlySpaces.ts
@@ -1,3 +1,11 @@
+/**
+ * By default, `URLSearchParams.toString()` encodes spaces as `+`. In some mail
+ * clients, this results in the `+`s being displayed literally, rather than
+ * being converted into a space as they should be.
+ *
+ * This helper converts `+`s to `%20`s, which seem to be more friendly to mail
+ * clients.
+ */
 export default function toStringWithMailFriendlySpaces(
   params: URLSearchParams
 ) {
